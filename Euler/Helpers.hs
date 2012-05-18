fe6b7ab33425a690ca2d3dfe215ba0e5f5ffa1af
Martin <kane@tyler.(none)>
2012-05-18 23:48:51 +0200

    Fixed toDigits function

--- Euler/Helpers.hs
+++ Euler/Helpers.hs
@@ -3,10 +3,11 @@ module Euler.Helpers (
  ,primes
  ,isPrime
  ,fromDigits
+ ,divisors
 ) where
 
 toDigits 0 = []
-toDigits n = (num n):(toDigits (n `div` 10))
+toDigits n = (toDigits (n `div` 10))++[(num n)]
     where
         num n = n - (n `div` 10)*10
 fromDigits xs = foldr1 d (reverse xs)
@@ -15,4 +16,4 @@ primes = 2:[x|x<-[3,5..], isPrime x]
 isPrime n = fst (head (divisors n)) == 1
 sqr n = floor (sqrt (fromInteger n))
 
-divisors n = [(x, x `div` n) | x<-[sqr n, sqr n - 1..1], n `mod` x == 0]
+divisors n = [(x, n `div` x) | x<-[sqr n, sqr n - 1..1], n `mod` x == 0]
