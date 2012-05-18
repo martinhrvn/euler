10bf3801aa059fc29d3c2d48efe5cdfc53d313b8
Martin Harvan <martin.harvan@navteq.com>
2012-05-18 23:16:34 +0200

    Added more conditions for problem 35

--- problem35.hs
+++ problem35.hs
@@ -4,5 +4,8 @@ shifts [] = []
 shifts xs = L.nub (zipWith concat (L.tails xs) (L.inits xs))
     where 
         concat a b = a ++ b
-problem35 = [x | x<-primes, x<1000000, all (isPrime) (map fromDigits (shifts (toDigits x)))]
-main = print (problem35)
+problem35 = 2:3:5:[x | x<-(takeWhile (<1000000) primes), (sum (toDigits x)) `mod` 3 /= 0, notEq [2,4,5,6,8,0] (toDigits x), all (isPrime) (map fromDigits (shifts (toDigits x)))]
+    where 
+       notEq [] ys = True 
+       notEq (c:cs) ys = all (/=c) ys && notEq cs ys
+main = print (length problem35)
